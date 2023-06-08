7de0abb9b7d4d5a320452fbe1a8c8a59e3e40d36
farima4 <jovozivanovic1995@gmail.com>
2023-06-08 13:25:51 +0200

    good day nigth fr

--- game/Objects/Props/day_night.gd
+++ game/Objects/Props/day_night.gd
@@ -1,39 +1,60 @@
 extends Node3D
 
 signal day_night_status_change(status)
+signal day_night_cycle_length(DawnDusk, DayNight)
 @onready var SunMoon = $SunMoon
+
+@onready var Dawn = $Dawn
 @onready var Day = $Day
+@onready var Dusk = $Dusk
 @onready var Night = $Night
-@export var time = 20
-# 0 - day
-# 1 - night
+
+@export var time = 360
+# 0 - dawn
+# 1 - day
+# 2 - dusk
+# 3 - night
 var status = 0
 
+@onready var DawnDusk = time * 0.1
+@onready var DayNight = time * 0.4
 
 func _ready():
-	$SunMoon/Sun2.cast_shadow = false
-	$SunMoon/Moon2.cast_shadow = false
-	print(Day.wait_time)
-	Day.wait_time = time / 2
-	Night.wait_time = time / 2
+	day_night_cycle_length.emit(DawnDusk, DayNight)
+	$Dawn.wait_time = DawnDusk
+	$Day.wait_time = DayNight
+	$Dusk.wait_time = DawnDusk
+	$Night.wait_time = DayNight
 	day_night_status_change.emit(status)
 	start()
 	
 func start():
 	var tween = get_tree().create_tween()
-	tween.tween_property(SunMoon, "rotation", Vector3(deg_to_rad(-180), 0, 0), time / 2).as_relative()
-	Day.start()
+	tween.tween_property(SunMoon, "rotation", Vector3(deg_to_rad(-360), 0, 0), time).as_relative()
+	Dawn.start()
 
-func _on_day_timeout():
-	var tween = get_tree().create_tween()
-	tween.tween_property(SunMoon, "rotation", Vector3(deg_to_rad(-180), 0, 0), time / 2).as_relative()
+func _on_dawn_timeout():
 	status = 1
 	day_night_status_change.emit(status)
+	Day.start()
+	
+func _on_day_timeout():
+	status = 2
+	day_night_status_change.emit(status)
+	Dusk.start()
+	
+func _on_dusk_timeout():
+	status = 3
+	day_night_status_change.emit(status)
 	Night.start()
-
-
 
 func _on_night_timeout():
 	status = 0
 	day_night_status_change.emit(status)
 	start()
+
+
+
+
+
+
--- game/Worlds/Fog.gd
+++ game/Worlds/Fog.gd
@@ -1,8 +1,19 @@
 extends WorldEnvironment
 
+var DawnDusk
+var DayNight
+
 func _on_day_night_day_night_status_change(status):
-	var is_day = false if status else true
+	if status == 1 or status == 3:
+		return 0
+	var is_Dawn = !status
 	var tween = get_tree().create_tween()
-	environment.fog_enabled = !is_day
-	tween.tween_property(environment, "background_energy_multiplier", 1 if is_day else 0.02, 4)
-	tween.tween_property(environment, "fog_density", 0 if is_day else 0.1, 5)
+	#environment.fog_enabled = !is_Dawn
+	#tween.tween_property()
+	tween.tween_property(environment, "background_energy_multiplier", 1 if is_Dawn else 0.02, DawnDusk)
+	tween.tween_property(environment, "fog_density", 0 if is_Dawn else 0.1, DawnDusk)
+
+
+func _on_day_night_day_night_cycle_length(_DawnDusk, _DayNight):
+	DawnDusk = _DawnDusk
+	DayNight = _DayNight
